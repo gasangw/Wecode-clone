39b7084b2ca06fee8b26195827faec5b2bb3a767
Gasangwa Thomas <thomasgasangwa@gmail.com>
2023-03-01 15:26:02 +0200

    Fetch data from an API and filter according to the Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default class App extends Component {
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      users:  [],
+      isloading: true,
+      filterData: '',
+
+    }
+    // console.log(typeof(this.state.filterData))
+  }
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then(nameList => {
+        console.log(nameList)
+          this.setState({ users: nameList, isloading: false });
+      })
+    }
   render() {
+    if(this.state.isloading){
+        return <h1>Loading...</h1>
+    }
     return (
-      <div>
-        <p>Hello Thomas</p>
+      <div className='container'>
+        <h1 className='title'>Search for someone</h1>
+        <input type='text' onChange={(e) => this.setState({filterData: e.target.value.toLocaleLowerCase()}) } placeholder='what are you searching for' />
+        {
+          this.state.users.filter((value) => {
+            return value.name.toLowerCase().includes(this.state.filterData)
+          }).map((data) => {
+            return (
+              <div key={data.id} className='data'>
+               <div className='card bg-dark'>
+                  <h3><span>Name:</span> {data.name}</h3>
+                  <p><span>Email:</span> {data.email}</p>
+               </div>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
 }
+
+export default App;
+
